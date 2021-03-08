fb86f63de26c1a6ac2367ebbe3ff3048f0283252
Rob Lang <brainwiped@gmail.com>
2021-03-08 22:30:57 +0000

    Added menu text, tested

--- Assets/Domain/Yellowy/Yellowy.cs
+++ Assets/Domain/Yellowy/Yellowy.cs
@@ -15,11 +15,11 @@ namespace Lang.ChainLetterJam
         void Update()
         {
             direction = 0;
-            if (Input.GetKey(KeyCode.A))
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
             {
                 direction = 1;
             }
-            else if (Input.GetKey(KeyCode.D))
+            else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
             {
                 direction = -1;
             }
